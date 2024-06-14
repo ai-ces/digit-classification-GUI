ba9479f38cfeb2ff0fe7cd60e1509ad6f22fef6f
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-14 06:41:31 +0300

    keras

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from PyQt5.QtCore import Qt
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg # load image
+
+import cv2 #open-cv use for image resize
+
+import keras # keras tensorflow
+from keras.datasets import mnist
+from keras.models import Sequential,load_model
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
