70a48aa6e675a8fed6b88d68a1b96e7c5a5b4bbf
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-14 06:28:45 +0300

    import

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap # icon and load image
 from PyQt5.QtCore import Qt
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg # load image
